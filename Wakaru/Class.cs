5cdb27adc0d5268f20754e0a7c34dc3151ef919c
TuRou <chenbohan911@163.com>
2022-02-07 13:08:12 +0800

    添加项目文件。

---
+++ Wakaru/Class.cs
@@ -0,0 +1,154 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wakaru
+{
+    internal class Class
+    {
+        readonly static string CLASS_BEGIN_RING_PATH = Path.Combine(Directory.GetCurrentDirectory(), "begin.wav");
+        readonly static string CLASS_OVER_RING_PATH = Path.Combine(Directory.GetCurrentDirectory(), "over.wav");
+        readonly static string CLASS_REST_BEGIN_RING_PATH = Path.Combine(Directory.GetCurrentDirectory(), "rest_begin.wav");
+        readonly static string CLASS_REST_OVER_RING_PATH = Path.Combine(Directory.GetCurrentDirectory(), "rest_over.wav");
+
+        public static readonly List<Class> CLASSES = new()
+        {
+            new Class(7, 10, 80),
+            new Class(8, 40, 80),
+            new Class(10, 10, 80),
+            new Class(13, 20, 80),
+            new Class(15, 10, 80),
+            new Class(18, 0, 80),
+            new Class(19, 30, 90, false),
+        };
+
+        public int BeginHour { get; set; }
+        public int BeginMinute { get; set; }
+        public int TotalMinute { get; set; }
+        public int MinutesToRest { get; set; } = 35;
+        public int RestInterval { get; set; } = 10;
+        public bool Rest { get; set; }
+        public Class(int hour, int minute, int totalMinute, bool rest = true)
+        {
+            BeginHour = hour;
+            BeginMinute = minute;
+            TotalMinute = totalMinute;
+            Rest = rest;
+            if (!rest)
+            {
+                MinutesToRest = 0;
+                RestInterval = 0;
+            } 
+        }
+
+        public IScheduler AddToScheduler(IScheduler scheduler)
+        {
+            var cur = DateTime.Now;
+            var date = new DateTime(cur.Year, cur.Month, cur.Day, BeginHour, BeginMinute, 0);
+            //上课
+            {
+                var jobDetail = JobBuilder.Create<ClassBeginRingJob>().Build();
+                ITrigger trigger = TriggerBuilder.Create()
+                    .WithSchedule(CronScheduleBuilder.DailyAtHourAndMinute(date.Hour, date.Minute))
+                    .Build();
+                scheduler.ScheduleJob(jobDetail, trigger);
+            }
+            //休息开始
+            if (Rest)
+            {
+                date = date.AddMinutes(MinutesToRest); 
+                var jobDetail = JobBuilder.Create<RestBeginRingJob>().Build();
+                ITrigger trigger = TriggerBuilder.Create()
+                    .WithSchedule(CronScheduleBuilder.DailyAtHourAndMinute(date.Hour, date.Minute))
+                    .Build();
+                scheduler.ScheduleJob(jobDetail, trigger);
+            }
+            //休息结束
+            if (Rest)
+            {
+                date = date.AddMinutes(RestInterval);
+                var jobDetail = JobBuilder.Create<RestOverRingJob>().Build();
+                ITrigger trigger = TriggerBuilder.Create()
+                    .WithSchedule(CronScheduleBuilder.DailyAtHourAndMinute(date.Hour, date.Minute))
+                    .Build();
+                scheduler.ScheduleJob(jobDetail, trigger);
+            }
+            //下课
+            {
+                date = date.AddMinutes(TotalMinute - RestInterval - MinutesToRest);
+                var jobDetail = JobBuilder.Create<ClassOverRingJob>().Build();
+                ITrigger trigger = TriggerBuilder.Create()
+                    .WithSchedule(CronScheduleBuilder.DailyAtHourAndMinute(date.Hour, date.Minute))
+                    .Build();
+                scheduler.ScheduleJob(jobDetail, trigger);
+            }
+            return scheduler;
+        }
+
+        private class ClassBeginRingJob : IJob
+        {
+            public Task Execute(IJobExecutionContext context)
+            {
+                return Task.Factory.StartNew(() =>
+                {
+                    MainWindow.AddLog("上课: " + DateTime.Now.ToString());
+                    MainWindow.NextTime = DateTime.Now.AddMinutes(35);
+                    MainWindow.ChangeStatus(Status.IN_CLASS);
+                    new SoundPlayer(CLASS_BEGIN_RING_PATH).Play();
+                });
+            }
+        }
+
+        private class ClassOverRingJob : IJob
+        {
+            public Task Execute(IJobExecutionContext context)
+            {
+                return Task.Factory.StartNew(() =>
+                {
+                    MainWindow.ClearLog();
+                    MainWindow.AddLog("下课: " + DateTime.Now.ToString());
+                    //课间休息
+                    MainWindow.NextTime = DateTime.Now.AddMinutes(10);
+                    //中午休息
+                    if (DateTime.Now.Hour == 11) MainWindow.NextTime = DateTime.Now.AddMinutes(110);
+                    //晚上休息
+                    if (DateTime.Now.Hour == 16) MainWindow.NextTime = DateTime.Now.AddMinutes(90);
+                    MainWindow.ChangeStatus(Status.CLASS_OVER);
+                    new SoundPlayer(CLASS_OVER_RING_PATH).Play();
+                });
+            }
+        }
+
+        private class RestBeginRingJob : IJob
+        {
+            public Task Execute(IJobExecutionContext context)
+            {
+                return Task.Factory.StartNew(() =>
+                {
+                    MainWindow.AddLog("课中休息开始: " + DateTime.Now.ToString());
+                    MainWindow.NextTime = DateTime.Now.AddMinutes(10);
+                    MainWindow.ChangeStatus(Status.RESTING);
+                    new SoundPlayer(CLASS_REST_BEGIN_RING_PATH).Play();
+                });
+            }
+        }
+        private class RestOverRingJob : IJob
+        {
+            public Task Execute(IJobExecutionContext context)
+            {
+                return Task.Factory.StartNew(() =>
+                {
+                    MainWindow.AddLog("课中休息结束: " + DateTime.Now.ToString());
+                    MainWindow.NextTime = DateTime.Now.AddMinutes(35);
+                    MainWindow.ChangeStatus(Status.AFTER_RESTING);
+                    new SoundPlayer(CLASS_REST_OVER_RING_PATH).Play();
+                });
+            }
+        }
+    }
+}
---
+++ Wakaru/MainWindow.xaml.cs
@@ -0,0 +1,133 @@
+﻿using Quartz.Impl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Wakaru
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static MainWindow Instance;
+        public static DateTime NextTime;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Instance = this;
+            AddLog("Wakaru正在启动...");
+            LoadClasses();
+            ChangeStatus(Status.CLASS_OVER);
+            DispatcherTimer dispatcherTimer = new()
+            {
+                Interval = new TimeSpan(0, 0, 0, 1, 0)
+            };
+            dispatcherTimer.Tick += new EventHandler((o, e) => {
+                Instance.Dispatcher.BeginInvoke(new Action(() => {
+                    Instance.TimeNowText.Text = "当前时间: " + DateTime.Now.ToString();
+                    Instance.TimeNextText.Text = "";
+                    Instance.TimeIntervalText.Text = "";
+                    if (NextTime != DateTime.MinValue) 
+                    {
+                        Instance.TimeNextText.Text = "下一时间: " + NextTime.ToString();
+                        Instance.TimeIntervalText.Text = "剩余时间: " + new TimeSpan(NextTime.Ticks - DateTime.Now.Ticks).ToString(@"hh\:mm\:ss");
+                    }
+                    
+                }));
+            });
+            dispatcherTimer.Start();
+        }
+        public async static void LoadClasses()
+        {
+            var schedulerFactory = new StdSchedulerFactory();
+            var scheduler = await schedulerFactory.GetScheduler();
+            foreach (var item in Class.CLASSES)
+            {
+                scheduler = item.AddToScheduler(scheduler);
+            }
+            AddLog("加载了" + Class.CLASSES.Count + "个课节, 时间: " + DateTime.Now.ToString());
+            await scheduler.Start();
+        }
+
+        public static void ChangeStatus(Status status)
+        {
+            Instance.Dispatcher.BeginInvoke(new Action(() => {
+                Instance.StateNowText.Text = "当前状态: " + GetStatusString(status);
+                Instance.StateNextText.Text = "下一状态: " + GetStatusString(status + 1);
+                if (status == Status.IN_CLASS || status == Status.AFTER_RESTING)
+                {
+                    Instance.ClassStateText.Text = "上课中";
+                    Instance.IconPic.Kind = MaterialDesignThemes.Wpf.PackIconKind.Book;
+                }
+                if (status == Status.RESTING)
+                {
+                    Instance.ClassStateText.Text = "休息中";
+                    Instance.IconPic.Kind = MaterialDesignThemes.Wpf.PackIconKind.Eye;
+                }
+                if (status == Status.CLASS_OVER)
+                {
+                    Instance.ClassStateText.Text = "下课中";
+                    Instance.IconPic.Kind = MaterialDesignThemes.Wpf.PackIconKind.ExitRun;
+                }
+                if (status == Status.WAIT_FOR_CLASS)
+                {
+                    Instance.ClassStateText.Text = "等待上课中";
+                    Instance.IconPic.Kind = MaterialDesignThemes.Wpf.PackIconKind.Sleep;
+                }
+            }));
+        }
+
+        public static void AddLog(string log)
+        {
+            Instance.Dispatcher.BeginInvoke(new Action(() => {
+                Instance.LogText.Text = Instance.LogText.Text + log + "\n";
+            }));
+        }
+
+        public static void ClearLog()
+        {
+            Instance.Dispatcher.BeginInvoke(new Action(() => {
+                Instance.LogText.Text = "";
+            }));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Instance.Close();
+        }
+
+        private void ColorZone_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Instance.DragMove();
+        }
+
+        private static string GetStatusString(Status status)
+        {
+            if (status == Status.IN_CLASS) return "上半节课";
+            if (status == Status.RESTING) return "课中休息";
+            if (status == Status.AFTER_RESTING) return "下半节课";
+            if (status == Status.CLASS_OVER) return "下课";
+            if (status == Status.WAIT_FOR_CLASS) return "上课";
+            return "未知";
+        }
+    }
+
+    public enum Status
+    {
+        IN_CLASS, RESTING, AFTER_RESTING, CLASS_OVER, WAIT_FOR_CLASS
+    }
+}
